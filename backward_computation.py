ff8576a3bd5446f61e750be7cc48f384a393f3ad
haretaro <frontend.1000@gmail.com>
2015-11-10 16:39:14 +0900

    add backward_computation.py

---
+++ backward_computation.py
@@ -0,0 +1,20 @@
+import numpy as np
+from chainer import Variable
+import chainer.functions as F
+
+#3入力2つのミニバッチ
+x = Variable(np.array([[1,1,1],[2,2,2]],dtype=np.float32))
+
+#出力関数
+y = x**2 -2*x + 1
+
+#入力が配列の時は初期誤差を手動で設定する
+y.grad = np.ones((2,3), dtype=np.float32)
+
+#誤差逆伝搬で勾配を計算
+y.backward()
+
+#勾配を表示.
+#dy = 2*x - 2 なので
+#[[0,0,0],[2,2,2]]になるはず
+print('grad',x.grad)
